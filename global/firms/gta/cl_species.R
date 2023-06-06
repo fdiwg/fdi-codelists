1e4afb0cf6a14d38431558b0157539a05e9542c6
eblondel <emmanuel.blondel1@gmail.com>
2023-06-06 18:30:09 +0200

    #21 set asfis subset codelists for level 0 and level 1

--- global/firms/gta/cl_species.R
+++ global/firms/gta/cl_species.R
@@ -1,5 +1,11 @@
 require(readr)
 asfis = readr::read_csv("https://raw.githubusercontent.com/fdiwg/fdi-codelists/main/global/cwp/cl_asfis_species.csv")
+
+#species targeted for GTA level 0
 gta_list = readr::read_csv("GTA_SPECIES_LIST_DRAFT.csv")
-gta_asfis = asfis[asfis$code %in% gta_list$`ASFIS code`,]
-readr::write_csv(gta_asfis, "cl_species.csv")
+gta_asfis_level0 = asfis[asfis$code %in% gta_list$`ASFIS code`,]
+readr::write_csv(gta_asfis_level0, "cl_species_level0.csv")
+
+#species targeted for GTA level 1
+gta_asfis_level1 = asfis[asfis$code %in% c("BET","SKJ","YFT","ALB","BFT","PBF","SBF","SWO"),]
+readr::write_csv(gta_asfis_level1, "cl_species_level1.csv")
