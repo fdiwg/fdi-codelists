3d744584e81575e2df27e7c6a2a26c4880db13cc
eblondel <emmanuel.blondel1@gmail.com>
2023-06-22 22:50:27 +0200

    update IOTC areas

--- global/firms/gta/cl_task1_iotc_areas.R
+++ global/firms/gta/cl_task1_iotc_areas.R
@@ -1,18 +1,17 @@
 setwd("D:/Documents/DEV/fdiwg/fdi-codelists")
-require(ows4R)
 
-WFS_FAO = WFSClient$new(
-	url = "https://www.fao.org/fishery/geoserver/fifao/wfs",
-	serviceVersion = "1.0.0",
-	logger = "INFO"
-)
-fao_major_areas = WFS$getFeatures("fifao:FAO_AREAS", cql_filter = URLencode("F_CODE IN('51','57')"))
-fao_major_areas$code = paste0("F", fao_major_areas$F_CODE)
-fao_major_areas$uri = NA
-fao_major_areas$label = fao_major_areas$NAME_EN
-fao_major_areas$definition = NA
-fao_major_areas$geom_wkt = sf::st_as_text(fao_major_areas$the_geom)
-fao_major_areas = as.data.frame(fao_major_areas[,c("code", "uri", "label", "definition", "geom_wkt")])
-fao_major_areas$the_geom = NULL
+download.file("https://data.iotc.org/reference/1.0.0/domain/admin/shapefiles/IOTC_MAIN_AREAS_1.0.0_SHP.zip", destfile = "IOTC_MAIN_AREAS_1.0.0_SHP.zip", mode = "wb")
+zipr::unzip("IOTC_MAIN_AREAS_1.0.0_SHP.zip")
 
-readr::write_csv(fao_major_areas, "global/firms/gta/cl_task1_iotc_areas.csv")
+sf::sf_use_s2(FALSE)
+iotc_areas = sf::st_read("IOTC_MAIN_AREAS_1.0.0.shp")
+
+iotc_areas$code = iotc_areas$CODE
+iotc_areas$uri = NA
+iotc_areas$label = iotc_areas$NAME_EN
+iotc_areas$definition = NA
+iotc_areas$geom_wkt = sf::st_as_text(iotc_areas$geometry)
+iotc_areas = as.data.frame(iotc_areas[,c("code", "uri", "label", "definition", "geom_wkt")])
+iotc_areas$geometry = NULL
+
+readr::write_csv(iotc_areas, "global/firms/gta/cl_task1_iotc_areas.csv")
