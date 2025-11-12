73908785327127423774083cc750acfce7ee9e26
eblondel <emmanuel.blondel1@gmail.com>
2025-11-12 22:04:14 +0100

    update iccat gis layers

--- regional/iccat/iccat_gis_layers.R
+++ regional/iccat/iccat_gis_layers.R
@@ -12,6 +12,10 @@ iccat_sa = do.call("rbind", lapply(iccat_sa_layers$name, function(l){
   sfl = sfl[,c("CODE","NAME_EN","NAME_ES","NAME_FR","stock")]
   return(sfl)
 }))
+#fix for YFT
+iccat_sa[iccat_sa$CODE %in% c("YF01","YF02","YF03","YF04","YF05","YF06","YF07","YF20"),]$stock = "YFT-E"
+iccat_sa[iccat_sa$CODE %in% c("YF08","YF09","YF10","YF11","YF12","YF13","YF30","YF40"),]$stock = "YFT-W"
+
 iccat_sa_billfish = sf::st_read(iccat_gis_gpkg, layer = "billfish_sampling_areas")
 stock_columns = colnames(iccat_sa_billfish )[startsWith(colnames(iccat_sa_billfish ),"stock")]
 iccat_sa_billfish_all = do.call("rbind", lapply(stock_columns, function(sc){
@@ -21,8 +25,9 @@ iccat_sa_billfish_all = do.call("rbind", lapply(stock_columns, function(sc){
 	return(sfl)
 }))
 
+
 iccat_sa_all = rbind(iccat_sa, iccat_sa_billfish_all)
-sf::st_write(iccat_sa_all, file.path("regional/iccat", "iccat_sampling_areas.gpkg"))
+sf::st_write(iccat_sa_all, file.path("regional/iccat", "iccat_sampling_areas.gpkg"), append = FALSE)
 
 iccat_stock_layers = iccat_layers[regexpr("stock_areas", iccat_layers$name)>0,]
 iccat_stocks = do.call("rbind", lapply(iccat_stock_layers$name, function(l){
@@ -35,7 +40,24 @@ iccat_stocks = do.call("rbind", lapply(iccat_stock_layers$name, function(l){
   sfl = sfl[,c("CODE","NAME_EN","NAME_ES","NAME_FR")]
   return(sfl)
 }))
-sf::st_write(iccat_stocks, file.path("regional/iccat", "iccat_stocks.gpkg"))
+#fix for YFT
+iccat_stocks = iccat_stocks[iccat_stocks$CODE != "YFT-A",]
+yft_w = sf::st_sf(
+	CODE = "YFT-W",
+	NAME_EN = "Yellowfin tuna stock area YFT-W",
+	NAME_ES = "Zona de stock de rabil YFT-W",
+	NAME_FR = "Zone de stock de l'albacore YFT-W",
+	geom = sf::st_union(iccat_sa[iccat_sa$stock == "YFT-W",])
+)#shows some issues with sampling_areas geoms
+yft_e = sf::st_sf(
+	CODE = "YFT-E",
+	NAME_EN = "Yellowfin tuna stock area YFT-E",
+	NAME_ES = "Zona de stock de rabil YFT-E",
+	NAME_FR = "Zone de stock de l'albacore YFT-E",
+	geom = sf::st_union(iccat_sa[iccat_sa$stock == "YFT-E",])
+)#shows some issues with sampling_areas geoms
+iccat_stocks = rbind(iccat_stocks, yft_e, yft_w)
+sf::st_write(iccat_stocks, file.path("regional/iccat", "iccat_stocks.gpkg"), append = F)
 
 setwd("../fdi-mappings/regional-to-regional/iccat")
 
