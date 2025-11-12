0ad216ac61d5a41c151cf438f72cf3163ce0444a
eblondel <emmanuel.blondel1@gmail.com>
2025-11-12 16:33:47 +0100

    update iccat sampling areas to reflect all possible mappings between billfish sampling areas and stock codes

--- regional/iccat/iccat_gis_layers.R
+++ regional/iccat/iccat_gis_layers.R
@@ -5,15 +5,25 @@
 iccat_gis_gpkg = "D:/Downloads/ICCAT_gis/ICCAT_gis.gpkg"
 iccat_layers = sf::st_layers(iccat_gis_gpkg)
 
-iccat_sa_layers = iccat_layers[regexpr("sampling_areas", iccat_layers$name)>0,]
+iccat_sa_layers = iccat_layers[regexpr("sampling_areas", iccat_layers$name)>0 & iccat_layers$name != "billfish_sampling_areas",]
 
 iccat_sa = do.call("rbind", lapply(iccat_sa_layers$name, function(l){
   sfl = sf::st_read(iccat_gis_gpkg, layer = l)
-  if("stock_oth" %in% colnames(sfl)) sfl$stock = sfl$stock_oth
   sfl = sfl[,c("CODE","NAME_EN","NAME_ES","NAME_FR","stock")]
   return(sfl)
 }))
-sf::st_write(iccat_sa, file.path("regional/iccat", "iccat_sampling_areas.gpkg"))
+iccat_sa_billfish = sf::st_read(iccat_gis_gpkg, layer = "billfish_sampling_areas")
+stock_columns = colnames(iccat_sa_billfish )[startsWith(colnames(iccat_sa_billfish ),"stock")]
+iccat_sa_billfish_all = do.call("rbind", lapply(stock_columns, function(sc){
+	sfl = sf::st_read(iccat_gis_gpkg, layer = "billfish_sampling_areas")
+	print(colnames(sfl))
+	sfl$stock = sfl[[sc]]
+	sfl = sfl[,c("CODE","NAME_EN","NAME_ES","NAME_FR","stock")]
+	return(sfl)
+}))
+
+iccat_sa_all = rbind(iccat_sa, iccat_sa_billfish_all)
+sf::st_write(iccat_sa_all, file.path("regional/iccat", "iccat_sampling_areas.gpkg"))
 
 iccat_stock_layers = iccat_layers[regexpr("stock_areas", iccat_layers$name)>0,]
 iccat_stocks = do.call("rbind", lapply(iccat_stock_layers$name, function(l){
